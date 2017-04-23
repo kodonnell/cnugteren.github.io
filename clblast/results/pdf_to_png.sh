ae5f5a5d1149ea2c16e0343318f4b36434d56557
Cedric Nugteren <web@cedricnugteren.nl>
2017-04-23 20:27:50 +0200

    Added CLBlast benchmarks for 6 devices

---
+++ clblast/results/pdf_to_png.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for DIR in *; do
+	if [[ -d ${DIR} ]]; then
+		cd ${DIR}
+
+		for FILE in *.pdf
+		do
+			convert -trim ${FILE} "`basename ${FILE} .pdf`.png"
+		done
+
+		for EXTRADIR in *; do
+			if [[ -d ${EXTRADIR} ]]; then
+				cd ${EXTRADIR}
+
+				for FILE in *.pdf
+				do
+					convert -trim ${FILE} "`basename ${FILE} .pdf`.png"
+				done
+
+				cd ..
+			fi
+		done
+		cd ..
+	fi
+done
